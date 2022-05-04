cc63877315713a73091668c165a92ebf2ff4276c
Robert Williams <robert@wilco-records.com>
2022-05-04 05:41:31 -0500

    Implemented basic navigation.

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
-ReactDOM.render(<p>React Project</p>, document.getElementById("root"))
+function Navbar() {
+    return (
+        <nav className="navbar navbar-expand-lg navbar-light bg-light">
+            <div className="container-fluid">
+                <a className="navbar-brand" href="#">Navbar</a>
+                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
+                        aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                    <span className="navbar-toggler-icon"></span>
+                </button>
+                <div className="collapse navbar-collapse" id="navbarNav">
+                    <ul className="navbar-nav">
+                        <li className="nav-item">
+                            <a className="nav-link active" aria-current="page" href="#">Home</a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link" href="#">Features</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+    )
+}
+
+function MainContent() {
+    return (
+        <h1>First React project!</h1>
+    )
+}
+
+ReactDOM.render(
+    <div>
+        <Navbar />
+        <MainContent />
+    </div>,
+    document.getElementById("root"))
